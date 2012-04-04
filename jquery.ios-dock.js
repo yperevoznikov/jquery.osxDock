a2d0cd36dda300163810b0cdbdf9b6c4f1d6bfca
Yuri Perevoznikov <yuriless@gmail.com>
2012-04-04 13:41:27 +0400

    initiate debug files

---
+++ jquery.ios-dock.js
@@ -0,0 +1,41 @@
+(function($){
+
+	$.fn.extend({ 
+
+/*
+        //This is where you write your plugin's name
+        pluginname: function() {
+ 
+            //Iterate over the current set of matched elements
+            return this.each(function() {
+             
+                //code to be inserted here
+             
+            });
+        }
+*/	
+/*
+$("#special").click(function(e){
+
+
+            //Settings list and the default values
+            var defaults = {
+                animatePadding: 60,
+                defaultPadding: 10,
+                evenColor: '#ccc',
+                oddColor: '#eee'
+            };
+             
+            var options = $.extend(defaults, options);
+
+	var x = e.pageX - this.offsetLeft;
+	var y = e.pageY - this.offsetTop;
+
+      $('#status2').html(x +', '+ y);
+   });
+*/
+
+
+	});
+
+})(jQuery);
